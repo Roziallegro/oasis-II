d156c48b49e78d8a10d18749072b95c3e32b9557
Roziallegro <rea.destor@gmail.com>
2023-07-28 11:43:33 +0800

    added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { Grid, GridItem, HStack, Image } from "@chakra-ui/react";
 import ColourModeSwitch from "./components/ColormodeSwitch";
+
+import logo from "./assets/study logo.png";
 
 function App() {
   return (
     <>
-      <ColourModeSwitch />
+      <Grid templateAreas={`"header" "main"`} gap="1">
+        <GridItem area={"header"}>
+          <HStack justifyContent="space-between">
+            <Image src={logo} boxSize="55px" objectFit="contain" />
+            <ColourModeSwitch />
+          </HStack>
+        </GridItem>
+        <GridItem bg="blue.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
